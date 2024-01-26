63715d7e0a74b5515eb62bb11d2098c16ec7b544
Carter Nichols <CarterN2500@gmail.com>
2024-01-26 11:08:57 -0500

    About layout complete

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,6 +1,39 @@
-
+import styles from './About.module.css'
 
 export default function About() {
-    <section>
-    </section>
+    return (
+        <section className={styles.container}>
+            <h2 className={styles.title}>About</h2>
+            <div className={styles.content}>
+                <img className={styles.headshot} src="Carter/Carter_Profile_Offical_Headshot.png" alt="Headshot" />
+                <ul className={styles.allItems}>
+                    <li className={styles.aboutItem}>
+                        <div className={styles.text}>
+                            <h3>Text Here</h3>
+                            <p>Explain Here</p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <div className={styles.text}>
+                            <h3>Text Here</h3>
+                            <p>Explain Here</p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <div className={styles.text}>
+                            <h3>Text Here</h3>
+                            <p>Explain Here</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    )
 }
+
+
+
+{/* <div className={styles.headshotContainer}>
+<h2 className={styles.title}>About</h2>
+<img className={styles.headshot} src="Carter/Carter_Profile_Offical_Headshot.png" alt="Headshot" />
+</div> */}
