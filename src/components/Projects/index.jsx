73fc88b7477db210be4861d148b227767a9951c2
Carter Nichols <CarterN2500@gmail.com>
2024-01-31 14:22:12 -0500

    Project cards complete

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,7 +1,36 @@
 import styles from './Projects.module.css'
+import projects from '../../data/projects.json'
 
 export default function Projects() {
     return (
-        <></>
+        <section className={styles.container}>
+            <h2 className={styles.title}>Projects</h2>
+            <div className={styles.projects}>
+                {
+                projects.map(function(project, id) {
+                    return(
+                        <div className={styles.content} key={id}>
+                            <img className={styles.image} src={project.imageSrc} alt={project.title} />
+                            <h3 className={styles.projTitle}>{project.title}</h3>
+                            <p className={styles.desc}>{project.desc}</p>
+                            <ul className={styles.techList}>
+                                {
+                                project.technology.map(function(tech, id) {
+                                    return (
+                                        <li key={id} className={styles.tech}>{tech}</li>
+                                    )
+                                })
+                                }
+                            </ul>
+                            <div className={styles.allLinks}>
+                                <a className={styles.link} href={project.demo}>Go to App</a>
+                                <a className={styles.link} href={project.source}>View the Code</a>
+                            </div>
+                        </div>
+                    )
+                })
+                }
+            </div>
+        </section>
     )
 }
