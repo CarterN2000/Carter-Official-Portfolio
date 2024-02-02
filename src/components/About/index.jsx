1b4d3d77bcca4a337709aa733ad60681ad1116e3
Carter Nichols <CarterN2500@gmail.com>
2024-02-01 23:58:27 -0500

    Changed fonts and fixed visual bug

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -10,21 +10,22 @@ export default function About() {
                     <li className={styles.aboutItem}>
                         <img className={styles.aboutIcon} src="Fun_Icons/Fullstack_Icon.png" alt="Icon_1"/>
                         <div className={styles.text}>
-                            <h3>I am a Full Stack Developer!</h3>
+                            <h3>I am a Full Stack Developer</h3>
                             <p>My skillset includes experience in both frontend and backend development. Personally, I prefer to work in the backend as I enjoy working with databases and APIs, but I can do it all!</p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
                         <img className={styles.aboutIcon} src="Fun_Icons/Psychology_Icon_2.png" alt="Icon_2"/>
                         <div className={styles.text}>
-                            <h3>I Originally had a Background in Psychology!</h3>
-                            <p>Before I found my passion for software development, I graduated from SUNY Brockport with a BS in Psychology. Although software engineering and psychology are very different, I believe having a well rounded background in multiple different fields helps me tackle problems from a unique point of view.</p>
+                            <h3>I Originally had a Background in Psychology</h3>
+                            <p>Before I found my passion for software development, I graduated from SUNY Brockport with a BS in Psychology, plus two minors in Forensic Science and Business Admin. Although software engineering and psychology are very different, I believe having a well rounded background in multiple different fields helps me tackle problems from a unique point of view.</p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
+                        <img className={styles.aboutIcon} src="Fun_Icons/sportsIcon.png" alt="Sports" />
                         <div className={styles.text}>
-                            <h3>My Parents are Divorced!</h3>
-                            <p>It was my fault!</p>
+                            <h3>I Enjoy Living an Active Lifestyle</h3>
+                            <p>Although I have a strong passion for full stack development, I also have many other hobbies. I am an avid sports fan, and enjoy spending my free time watching or playing a variety of sports. I also enjoy working out and traveling!</p>
                         </div>
                     </li>
                 </ul>
